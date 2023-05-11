9f35be4f05e2ba268ca580ec99f7663fecae8d20
CarolineMorton <carolinemorton@gmx.com>
2023-05-11 15:29:02 +0100

    add docstrings for functions

--- calculation.py
+++ calculation.py
@@ -1,7 +1,13 @@
 
 def add_five(x):
+    """
+    Adds 5 to the given number
+    """
     return x + 5
 
 
 def multiply_by_two(x: int):
+    """
+    Multiplies the given number by 2
+    """
     return x * 2
