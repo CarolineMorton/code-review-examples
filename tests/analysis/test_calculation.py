8cdd136c7a5be3a42fea4319359811e34d436691
CarolineMorton <carolinemorton@gmx.com>
2023-05-25 16:24:42 +0100

    feat(calculation.py): add functions to perform basic arithmetic operations on numbers
    Add two functions to the `calculation.py` module to perform basic arithmetic operations on numbers. The `add_five` function adds 5 to the given number, while the `multiply_by_two` function multiplies the given number by 2.
    
    test(calculation.py): add test for add_five function
    
    Add a test for the `add_five` function in the `test_calculation.py` module. The test checks if the function returns the expected result when given a specific input.
    
    test(e2e.py): add import statements and variables for testing data
    
    Add import statements and variables for testing data to the `test_e2e.py` module. These will be used to test the `join_data.py` script.

---
+++ tests/analysis/test_calculation.py
@@ -0,0 +1,7 @@
+# from analysis.calculation import add_five
+
+
+# def test_add_five():
+#     observed = add_five(10)
+#     assert observed == 15
+
---
+++ tests/test_e2e.py
@@ -0,0 +1,12 @@
+# import pandas as pd
+
+# from 
+# from cleaning.join_data import merge_events_with_demographics, add_bmi_to_data, add_copd_to_data, drop_duplicates, drop_unneeded_cols
+
+
+# EVENTS_PATH = "tests/test_data/events.csv"
+# DEMOGRAPHICS_PATH = "tests/test_data/demographics.csv"
+
+
+# def test_script():
+#     event_data 
